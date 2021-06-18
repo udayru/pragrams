2cd2b6d1b6de343b0702ea110dc0c96c6dcc48e6
udayru <uday9844@gmail.com>
2021-06-18 12:11:01 -0400

    18/6/21 second commit

--- queue.py
+++ queue.py
@@ -2,29 +2,55 @@ import queue
  
 class queue_class:
     
-    def __init__(self,items):
-        self.list_items = items
-        self.list_container = queue.Queue()   
+    def __init__(self,items,num):
+        if num == '1':
+            self.list_items = items
+            self.list_container = queue.Queue()
+            self.massage = 'FIFO'
+        elif num == '2':
+            self.list_items = items
+            self.list_container = queue.LifoQueue()
+            self.massage = 'FILO'
     def list_append(self,add):
         self.add = add
         self.list_container.put(add)
-        print(f'your value is add to queue : {add}')
+        print()
+        print(f'your value is add to queue ({self.massage}) : {add}')
     def list_pop(self):
         if self.list_container.empty():
-            print(f'there is no items in queue you can items pressing 1')
+            print()
+            print(f'there is no items in queue ({self.massage}) you can add items by  pressing 1')
         else:
+            print()
             rem =self.list_container.get()
-            print(f'your value is removed from the queue :  {rem}')
+            print(f'your value is removed from the queue ({self.massage}) :  {rem}')
         
     def container_print(self):
         if self.list_container.empty():
-            print(f"there is not value in queue {self.list_items}")
+            print()
+            print(f"there is no value in queue ({self.massage}) {self.list_items} you can chosse again ")
         else :
-            print(f' name is {self.list_items} and  {[i for i in self.list_container.queue]}')
-        
-name  = str(input("Enter the name of queue : "))
-name=queue_class(name)
+            print()
+            print(f'the name of queue ({self.massage}) : {self.list_items} = {[i for i in self.list_container.queue]}')
+ 
 
+
+
+
+
+
+
+
+num = str(input("""Enter 1 want to work on FIFI
+Enter 2 want to work on FIFO
+"""))
+if num == '1'or num == '2':
+    
+    name  = str(input("Enter the name of queue : "))
+    name=queue_class(name,num)
+else:
+    print("you enter invalid option ! please run again ")
+    exit()
  
 while True:
     print("""
@@ -49,16 +75,7 @@ Emter 4 to Exit from queue
     else:
         print("enter invalued value")
         continue
-        
-#     print("enter to q to exit and c to continue : ")
-#     ch_1=''
-#     while ch_1!= 'q' and ch_1 != 'c':
-#         ch_1 = input()
-#         if ch_1 =='q':
-#             exit()
-#         elif ch_1=='c':
-#             continue
-#             
+               
  
         
     
